0ae5c6cc77679f1e9a12fde7c808e185cb257996
tinnguyen03102004 <tinnguyen03102004@gmail.com>
2025-07-07 14:52:45 +0700

    Remove Guzzle dependency

--- helpers/ThumbnailFetcher.php
+++ helpers/ThumbnailFetcher.php
@@ -1,26 +1,25 @@
 <?php
-use GuzzleHttp\Client;
 
 class ThumbnailFetcher {
     public static function get($url) {
-        try {
-            $client = new Client(['timeout' => 7]);
-            $res = $client->get($url);
-            $html = (string)$res->getBody();
-            // Find og:image
-            if (preg_match('/<meta[^>]+property=[\'\"]og:image[\'\"][^>]+content=[\'\"]([^\'\"]+)[\'\"][^>]*>/i', $html, $m)) {
-                return $m[1];
-            }
-            // Find twitter:image
-            if (preg_match('/<meta[^>]+name=[\'\"]twitter:image[\'\"][^>]+content=[\'\"]([^\'\"]+)[\'\"][^>]*>/i', $html, $m)) {
-                return $m[1];
-            }
-            // Fallback: first <img>
-            if (preg_match('/<img[^>]+src=[\'\"]([^\'\"]+)[\'\"]/i', $html, $m)) {
-                return $m[1];
-            }
-        } catch (\Exception $e) {
+        $context = stream_context_create([
+            'http' => [
+                'timeout' => 7,
+                'user_agent' => 'Mozilla/5.0'
+            ]
+        ]);
+        $html = @file_get_contents($url, false, $context);
+        if ($html === false) {
             return null;
+        }
+        if (preg_match('/<meta[^>]+property=[\'\"]og:image[\'\"][^>]+content=[\'\"]([^\'\"]+)[\'\"][^>]*>/i', $html, $m)) {
+            return $m[1];
+        }
+        if (preg_match('/<meta[^>]+name=[\'\"]twitter:image[\'\"][^>]+content=[\'\"]([^\'\"]+)[\'\"][^>]*>/i', $html, $m)) {
+            return $m[1];
+        }
+        if (preg_match('/<img[^>]+src=[\'\"]([^\'\"]+)[\'\"]/i', $html, $m)) {
+            return $m[1];
         }
         return null;
     }
