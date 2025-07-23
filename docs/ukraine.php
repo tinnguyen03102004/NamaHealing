58b9bd1bd9ff610d067744a0cb90f0ca19087087
tinnguyen03102004 <tinnguyen03102004@gmail.com>
2025-07-23 11:00:05 +0700

    Add Ukraine meditation class link and page

---
+++ docs/ukraine.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = __('ukraine_class_title');
+include '../header.php';
+?>
+<main class="max-w-3xl mx-auto px-4 py-6">
+  <h1 class="text-2xl font-semibold mb-4"><?= __('ukraine_class_title') ?></h1>
+  <p><?= __('ukraine_class_desc') ?></p>
+</main>
+<?php include '../footer.php'; ?>
