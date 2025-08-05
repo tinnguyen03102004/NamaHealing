97c7e15c2d29e44f91cdaf2ddb6544c11b25ebbf
tinnguyen03102004 <tinnguyen03102004@gmail.com>
2025-08-05 17:00:03 +0700

    Remove out-of-sessions Zalo flow

--- join.php
+++ join.php
@@ -42,25 +42,5 @@ if ($remain > 0) {
     exit;
 }
 
-// Định dạng link Zalo chuẩn
-$zalo_1 = "https://zalo.me/0839269501";
-$zalo_2 = "https://zalo.me/0989399278";
-
-include 'header.php';
-?>
-<div class="d-flex justify-content-center align-items-center" style="min-height:60vh;">
-  <div class="card p-4 text-center" style="max-width:400px;">
-    <h3 class="mb-3 text-danger"><?= __('out_of_sessions') ?></h3>
-    <div class="mb-3 text-secondary">
-      <?= __('contact_admin') ?><br>
-    </div>
-    <a class="btn btn-success w-100 mb-2" href="<?= $zalo_1 ?>" target="_blank" rel="noopener">
-      <?= __('renew_zalo1') ?>
-    </a>
-    <a class="btn btn-success w-100 mb-2" href="<?= $zalo_2 ?>" target="_blank" rel="noopener">
-      <?= __('renew_zalo2') ?>
-    </a>
-    <a class="btn btn-secondary w-100" href="dashboard.php"><?= __('back_to_dashboard') ?></a>
-  </div>
-</div>
-<?php include 'footer.php'; ?>
+header('Location: welcome.php');
+exit;
