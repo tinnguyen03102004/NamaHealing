ec1de2da07c88e12baa071fbe25736735d52ba2d
tinnguyen03102004 <tinnguyen03102004@gmail.com>
2025-08-13 09:01:27 +0700

    Update articles.php

--- articles.php
+++ articles.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/i18n.php';
-$pageTitle = 'Bài Viết – Kiến Thức Thiền & Chữa Lành | NamaHealing';
+$pageTitle = 'Bài Viết';
 $metaDescription = 'Tổng hợp các bài viết chia sẻ kiến thức thiền định, yoga và tự chữa lành từ NamaHealing.';
 
 $file = __DIR__ . '/data/articles.json';
